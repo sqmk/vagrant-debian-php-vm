01f30fd359acff8f0e33e38845d17e7262d7a83c
Michael Squires <sqmk@php.net>
2013-06-30 22:32:12 -0700

    Add mysql

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,8 +1,23 @@
-file {'php.ini':
+file { 'php.ini':
   path    => '/tmp/php.ini.bak',
   ensure  => present,
   mode    => 0755,
   content => 'a value'
 } 
 
-class { 'apt': }
+class { 'apt':
+  always_apt_update => true
+}
+
+class mysql {
+
+  package { "mysql-server": 
+    ensure => installed 
+  }
+
+  service { "mysql":
+    ensure => running,
+  }
+}
+
+include mysql
